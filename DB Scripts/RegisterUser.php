766e2dd83e771510f14ae92ff5e505ff3284d663
southeja <81819499+southeja@users.noreply.github.com>
2021-10-16 14:17:54 -0600

    Added RegisterUser.php and query to db to register a new user

---
+++ DB Scripts/RegisterUser.php
@@ -0,0 +1,34 @@
+<?php
+
+    $SERVER_NAME = "localhost";
+    $DB_USERNAME = "id17730895_southeja";
+    $DB_PASSWORD = "6<>Cf<3&f4vqwAU8";
+    $DB = "id17730895_tcg";
+
+    $connection = mysqli_connect($SERVER_NAME, $DB_USERNAME, $DB_PASSWORD, $DB);
+
+    if (mysqli_connect_errno()) {
+        echo "Connection to database failed."
+        exit();
+    }
+
+    //Receieve user input username and password from POST request
+    $username = $_POST["username"];
+    $hashed_password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    //Query database to confirm username is not taken
+    $checkUsernameQuery = "SELECT username FROM users WHERE username='" . $username . "';";
+    $checkUsername = mysqli_query($connection, $checkUsernameQuery) or die("Check username query failed.");
+
+    //Make sure username is not already taken by another user
+    if (mysqli_num_rows($checkUsername) > 0) {
+        echo "Username already exists.";
+        exit();
+    }
+
+    //Query database to create new user in users table
+    $createUserQuery = "INSERT INTO users (username, password) VALUES ('" . $username . "', '" . $hashed_password . "');";
+    mysqli_query($connection, $createUserQuery) or die ("Create user query failed.");
+
+    echo("succes");
+?>
