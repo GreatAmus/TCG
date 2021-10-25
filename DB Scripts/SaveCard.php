e89f50c392c20bab7ef2e387ca06af06cced5af7
southeja <81819499+southeja@users.noreply.github.com>
2021-10-24 18:19:45 -0600

    Added save card script to webserver

---
+++ DB Scripts/SaveCard.php
@@ -0,0 +1,33 @@
+<?php
+    require('Config.php');
+
+    $connection = mysqli_connect($SERVER_NAME, $DB_USERNAME, $DB_PASSWORD, $DB);
+
+    if (mysqli_connect_errno()) {
+        echo "Connection to database failed.";
+        exit();
+    }
+
+    //Receieve card fields from POST
+    //$deckID = $_POST["deckID"];
+    $deckID = "";
+    $name = $_POST["name"];
+    $description = $_POST["description"];
+    //$imagePath = $_POST["imagePath"];
+    $imagePath = "";
+    $element = $_POST["element"];
+    $cost = $_POST["cost"];
+    $isSummon = $_POST["isSummon"];
+
+    if ($isSummon) {
+        $life = $_POST["life"];
+        $attack = $_POST["attack"];
+        $defense = $_POST["defense"];
+
+        //Query database to create new card in cards table
+        $createCardQuery = "INSERT INTO cards (name, description, imagePath, element, cost, isSummon, life, attack, defense) VALUES ('" . $name . "', '" . $description . "', '" . $imagePath . "', '" . $element . "', '" . $cost . "', '" . $isSummon . "', '" . $life . "', '" . $attack . "', '" . $defense . "');";
+        mysqli_query($connection, $createCardQuery) or die ("Create card query failed.");
+    }
+
+    echo("Success.");
+?>
